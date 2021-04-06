7f8703e75a7e80f3ef5cd802050fb9c0e7bd5f1b
Luis Alberto Mendoza <>
2021-04-06 11:36:56 +0200

    first commit

---
+++ prueba_app/prueba_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prueba_app
+//
+//  Created by avantgarde on 6/4/21.
+//  Copyright © 2021 Avantgarde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
